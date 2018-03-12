f8d5fc6fcd1b029f1c159431a3a2c131c230301e
Cheng Duan <lawrence.chengduan@gmail.com>
2018-03-11 20:14:50 -0400

    Approach 3 and main class

---
+++ 14.LongestCommonPrefix/main.java
@@ -0,0 +1,24 @@
+public class MainClass {
+    public static String[] stringToStringArray(String input) {
+        JsonArray jsonArray = JsonArray.readFrom(line);
+        String[] arr = new String[jsonArray.size()];
+        for (int i = 0; i < arr.length; i++) {
+          arr[i] = jsonArray.get(i).asString();
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String[] strs = stringToStringArray(line);
+
+            String ret = new Solution().longestCommonPrefix(strs);
+
+            String out = (ret);
+
+            System.out.print(out);
+        }
+    }
+}
