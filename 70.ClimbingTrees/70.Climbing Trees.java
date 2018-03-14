143625e6d5f42688f72f6d48ee1db80c72d10f81
Cheng Duan <lawrence.chengduan@gmail.com>
2018-03-14 20:13:01 +0000

    Accpedted solution

--- 70.ClimbingTrees/70.Climbing Trees.java
+++ 70.ClimbingTrees/70.Climbing Trees.java
@@ -24,3 +24,19 @@ Explanation:  There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 */
+
+//Accpeted
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n-1];
+    }
+}
