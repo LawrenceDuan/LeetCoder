5659ed90663d63a06d3d71daf24188488dbe7a58
Cheng Duan <lawrence.chengduan@gmail.com>
2018-03-14 20:49:44 +0000

    Fibonacci Solutions...awsome

--- 70.ClimbingTrees/70.Climbing Trees.java
+++ 70.ClimbingTrees/70.Climbing Trees.java
@@ -55,3 +55,29 @@ public class Solution {
         return memo[i];
     }
 }
+
+//Solution 1 Fibonacci Number
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
+
+//Solution 2 Fibnacci Fomular
+public class Solution {
+    public int climbStairs(int n) {
+        double sqrt5=Math.sqrt(5);
+        double fibn=Math.pow((1+sqrt5)/2,n+1)-Math.pow((1-sqrt5)/2,n+1);
+        return (int)(fibn/sqrt5);
+    }
+}
