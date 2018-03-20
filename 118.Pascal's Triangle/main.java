bdc9773a9cdb003cfa6a1dc9f69cae0ee5ca8697
Cheng Duan <lawrence.chengduan@gmail.com>
2018-03-20 17:52:59 +0000

    main class

---
+++ 118.Pascal's Triangle/main.java
@@ -0,0 +1,43 @@
+public class MainClass {
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int numRows = Integer.parseInt(line);
+
+            List<List<Integer>> ret = new Solution().generate(numRows);
+
+            String out = int2dListToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
