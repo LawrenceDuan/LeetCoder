815de717092834da620c5edb896ff65111fa829b
Cheng Duan <lawrence.chengduan@gmail.com>
2018-03-26 16:59:43 +0100

    Math solution. Very impressive method.

--- 136.SingleNumber/136. Single Number.java
+++ 136.SingleNumber/136. Single Number.java
@@ -23,3 +23,13 @@ class Solution {
         return singleNumber;
     }
 }
+
+//Solution Math
+//2∗(a+b+c)−(a+a+b+b+c)=c
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return 2 * sum(set(nums)) - sum(nums)
